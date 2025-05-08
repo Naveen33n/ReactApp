92f1d6b442638bab197278d19da745ef7df036ca
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-09 05:18:45 +0530

    ep1

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"hello world"),React.createElement("h2",{},"hello world")]));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
+
