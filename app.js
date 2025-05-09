62056c428beb16ea6a95449e4c91d57de6bed716
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-09 06:02:17 +0530

    ep2

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"hello world"),React.createElement("h2",{},"hello world")]));
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1",{},"hello")
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
 
