eeb929c2d8681977f06fa1507b2c9928768d35d2
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-09 14:50:21 +0530

    react functional components and jsx

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-import React from "react";
+ React
 import ReactDOM from "react-dom/client"
+import React from "react";
 
-const heading = React.createElement("h1",{},"hello")
+const Title=()=>{
+  return(
+    <div className="title">
+      <h1>title</h1>
+    </div>
+  )
+}
+const AppLayout=()=>(
+  <div>
+   <Title/>
+    <h1>hello</h1>
+    {Title()}
+    <Title></Title>
+  </div>
+)
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<AppLayout/>)
+
 
